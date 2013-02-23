41b046a97cc1bcc1e9436a0700436f45b2d50c6a
Karl Rosaen <krosaen@gmail.com>
2013-02-23 14:40:21 -0500

    tabs to spaces, whoops

--- sol.py
+++ sol.py
@@ -60,101 +60,107 @@ import logging
 logging.getLogger().setLevel(logging.INFO)
 
 def main():
+    n, k, start, goal = parseinput()
 
-	n, k, start, goal = parseinput()
+    logging.info("%s, %s", n, k)
+    logging.info("%s", start)
+    logging.info("%s", goal)
 
-	logging.info("%s, %s", n, k)
-	logging.info("%s", start)
-	logging.info("%s", goal)
+    pegstate = transform_to_pegs(start, k)
 
-	pegstate = transform_to_pegs(start, k)
-
-	moves = solve_best_first(pegstate, goal)
-	# moves = solve_breadth_first(pegstate, goal)
-	print len(moves)
-	for start, end in moves:
-		print "%s %s" % (start+1, end+1)
+    moves = solve_best_first(pegstate, goal)
+    # moves = solve_breadth_first(pegstate, goal)
+    print len(moves)
+    for start, end in moves:
+        print "%s %s" % (start + 1, end + 1)
 
 def parseinput():
-	thein = fileinput.input()
-	n, k = map(int, thein.next().split())
-	start = map(lambda el: int(el) - 1, thein.next().split())
-	goal = map(lambda el: int(el) - 1, thein.next().split())
-	return n, k, start, goal
+    thein = fileinput.input()
+    n, k = map(int, thein.next().split())
+    start = map(lambda el: int(el) - 1, thein.next().split())
+    goal = map(lambda el: int(el) - 1, thein.next().split())
+    return n, k, start, goal
+
 
 def transform_to_pegs(rep, k):
-	"""
-	rep: [int, int, ...] 
-		Each integer is in the range 0 to K-1 where the i-th integer denotes the peg to which disc of radius i is present in the initial configuration.
-	returns: [[], ...]
-		Each list represents radiuses on pegs. Transforms to zero
-		based offsets.
-	"""
-	result = [[] for i in xrange(k)]
-	for r, pos in reversed(list(enumerate(rep))):
-		result[pos].append(r)
-	return result
+    """
+     rep: [int, int, ...]
+         Each integer is in the range 0 to K-1 where the i-th integer denotes the peg to which disc of radius i is present in the initial configuration.
+     returns: [[], ...]
+         Each list represents radiuses on pegs. Transforms to zero
+         based offsets.
+     """
+    result = [[] for i in xrange(k)]
+    for r, pos in reversed(list(enumerate(rep))):
+        result[pos].append(r)
+    return result
+
 
 def solve_breadth_first(pegstate, goal):
-	"solves input01.txt after visiting 808 states"
-	moves = []
-	visits = 1
-	candidates = Queue()
+    "solves input01.txt after visiting 808 states"
+    moves = []
+    visits = 1
+    candidates = Queue()
 
-	observed_states = set()
-	moves = []
+    observed_states = set()
+    moves = []
 
-	while True:
-		lastmove = moves[-1] if moves else (None, None)
-		logging.info(
-			"%s via %s->%s (%s moves so far, %s candidates under consideration)",
-			pegstate, lastmove[0], lastmove[1], visits, len(candidates))
+    while True:
+        lastmove = moves[-1] if moves else (None, None)
+        logging.info(
+            "%s via %s->%s (%s moves so far, %s candidates under consideration)",
+            pegstate, lastmove[0], lastmove[1], visits, len(candidates))
 
-		if solved(pegstate, goal):
-			return moves
-		else:
-			observed_states.add(hashable_pegstate(pegstate))
-			visits += 1
-		for start, end in candidate_moves(pegstate):
-			next_state = apply_move(pegstate, start, end)
-			if hashable_pegstate(next_state) in observed_states:
-				continue
-			candidates.push((next_state, moves + [(start, end)]))
-		pegstate, moves = candidates.pop()
+        if solved(pegstate, goal):
+            return moves
+        else:
+            observed_states.add(hashable_pegstate(pegstate))
+            visits += 1
+        for start, end in candidate_moves(pegstate):
+            next_state = apply_move(pegstate, start, end)
+            if hashable_pegstate(next_state) in observed_states:
+                continue
+            candidates.push((next_state, moves + [(start, end)]))
+        pegstate, moves = candidates.pop()
+
 
 def solve_best_first(pegstate, goal):
-	"solves input01.txt after visiting 6 states!"
-	moves = []
-	visits = 0
-	priority_fn = make_sort_estimated_moves_lower_bound_fn(goal)
-	candidates = []
+    "solves input01.txt after visiting 6 states!"
+    moves = []
+    visits = 0
+    priority_fn = make_sort_estimated_moves_lower_bound_fn(goal)
+    candidates = []
 
-	moves = []
+    moves = []
 
-	while True:
-		lastmove = moves[-1] if moves else (None, None)
-		logging.info(
-			"%s via %s->%s (%s moves so far, %s candidates under consideration)",
-			pegstate, lastmove[0], lastmove[1], visits, len(candidates))
-		if solved(pegstate, goal):
-			return moves
-		else:
-			visits += 1
-		for start, end in candidate_moves(pegstate):
-			next_state = apply_move(pegstate, start, end)
-			candidates.append((next_state, moves + [(start, end)]))
-		candidates.sort(key=lambda el: -priority_fn(el))
-		pegstate, moves = candidates.pop()
+    while True:
+        lastmove = moves[-1] if moves else (None, None)
+        logging.info(
+            "%s via %s->%s (%s moves so far, %s candidates under consideration)",
+            pegstate, lastmove[0], lastmove[1], visits, len(candidates))
+        if solved(pegstate, goal):
+            return moves
+        else:
+            visits += 1
+        for start, end in candidate_moves(pegstate):
+            next_state = apply_move(pegstate, start, end)
+            candidates.append((next_state, moves + [(start, end)]))
+        candidates.sort(key=lambda el: -priority_fn(el))
+        pegstate, moves = candidates.pop()
+
 
 def hashable_pegstate(pegstate):
-	return tuple([tuple(peg) for peg in pegstate])
+    return tuple([tuple(peg) for peg in pegstate])
 
-class Queue(object): 
+
+class Queue(object):
     def __init__(self):
         self.in_stack = []
         self.out_stack = []
+
     def push(self, obj):
         self.in_stack.append(obj)
+
     def pop(self):
         if not self.out_stack:
             while self.in_stack:
@@ -162,63 +168,69 @@ class Queue(object):
         return self.out_stack.pop()
 
     def __len__(self):
-    	return len(self.in_stack) + len(self.out_stack)
+        return len(self.in_stack) + len(self.out_stack)
+
 
 def make_sort_estimated_moves_lower_bound_fn(goal):
-	def sort_key_fn(thetuple):
-		"""
-		Args:
-			thetuple: [pegstate, moves_so_far]
+    def sort_key_fn(thetuple):
+        """
+          Args:
+              thetuple: [pegstate, moves_so_far]
 
-		Cost estimated based on the difference between the pegstate
-		and the goal, along with the number of moves taken so far.
+          Cost estimated based on the difference between the pegstate
+          and the goal, along with the number of moves taken so far.
 
-		Using this for best first is essentially the 'A*' algorithm.
-		"""
-		pegstate, moves = thetuple
-		nummoves = len(moves)
-		estimated_moves_lower_bound = 0
-		for r, goalpos in enumerate(goal):
-			actual_pos = [i for i, peg in enumerate(pegstate) if r in peg][0]
+          Using this for best first is essentially the 'A*' algorithm.
+          """
+        pegstate, moves = thetuple
+        nummoves = len(moves)
+        estimated_moves_lower_bound = 0
+        for r, goalpos in enumerate(goal):
+            actual_pos = [i for i, peg in enumerate(pegstate) if r in peg][0]
 
-			if actual_pos == goalpos:
-				continue
+            if actual_pos == goalpos:
+                continue
 
-			goalpeg = pegstate[goalpos]
-			actualpeg = pegstate[actual_pos]
+            goalpeg = pegstate[goalpos]
+            actualpeg = pegstate[actual_pos]
 
-			estimated_moves_lower_bound += 1 # at least one move to get disc to goal peg
-			for disc in goalpeg:
-				if disc < r:
-					estimated_moves_lower_bound += 1  # at least one move for any disc smaller than r on goal peg
-			for disc in reversed(actualpeg):
-				if disc == r:
-					break
-				estimated_moves_lower_bound += 1 # at least one move for any disc on top of r on existing peg
+            estimated_moves_lower_bound += 1 # at least one move to get disc to goal peg
+            for disc in goalpeg:
+                if disc < r:
+                    estimated_moves_lower_bound += 1  # at least one move for any disc smaller than r on goal peg
+            for disc in reversed(actualpeg):
+                if disc == r:
+                    break
+                estimated_moves_lower_bound += 1 # at least one move for any disc on top of r on existing peg
 
-		return estimated_moves_lower_bound + nummoves
-	return sort_key_fn
+        return estimated_moves_lower_bound + nummoves
+
+    return sort_key_fn
+
 
 def solved(pegstate, goal):
-	for r, pos in enumerate(goal):
-		if r not in pegstate[pos]:
-			return False
-	return True
+    for r, pos in enumerate(goal):
+        if r not in pegstate[pos]:
+            return False
+    return True
+
 
 def top_of_pegs(pegstate):
-	for i, peg in enumerate(pegstate):
-		if len(peg):
-			yield i, peg[-1]
+    for i, peg in enumerate(pegstate):
+        if len(peg):
+            yield i, peg[-1]
+
 
 def candidate_moves(pegstate):
-	for i, top_r in top_of_pegs(pegstate):
-		for j, peg in enumerate(pegstate):
-			if i != j and (len(peg) == 0 or top_r < peg[-1]):
-				yield (i, j)
+    for i, top_r in top_of_pegs(pegstate):
+        for j, peg in enumerate(pegstate):
+            if i != j and (len(peg) == 0 or top_r < peg[-1]):
+                yield (i, j)
+
 
 def apply_move(pegstate, start, end):
-	result = [peg[:] for peg in pegstate]
-	result[end].append(result[start].pop())
-	return result
+    result = [peg[:] for peg in pegstate]
+    result[end].append(result[start].pop())
+    return result
 
 main()
